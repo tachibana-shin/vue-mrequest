70bb7cda97e76249bff59de591ade5c751fc1389
Tachibana Shin <tachibshin@duck.com>
2024-09-07 02:43:40 +0800

    add script build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import dts from "vite-plugin-dts"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.app.json"
+    })
   ],
   resolve: {
     alias: {
-      'src': fileURLToPath(new URL('./src', import.meta.url))
+      src: fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: "lib/index.ts",
+      formats: ["es"]
+    },
+    rollupOptions: {
+      external: ["@vueuse/core", "ts-retry", "lodash.get", "vue"]
     }
   }
 })
